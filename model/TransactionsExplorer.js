c70d75213e2ad577848724dbc0e95aee7fd20fcf
Samer Rahmeh <samgr93@gmail.com>
2023-04-12 03:51:44 +0300

    Update TransactionsExplorer.js

--- model/TransactionsExplorer.js
+++ model/TransactionsExplorer.js
@@ -73,10 +73,18 @@ define(["require", "exports", "./Transaction", "./MathUtil", "./Cn"], function (
             return extras;
         };
         TransactionsExplorer.isMinerTx = function (rawTransaction) {
-            if (rawTransaction.vin.length > 0 && rawTransaction.vin[0].type === 'ff') {
-                return true;
+            if (!Array.isArray(rawTransaction.vout) || rawTransaction.vin.length > 0)
+                return false;
+            if (!Array.isArray(rawTransaction.vout) || rawTransaction.vout.length === 0) {
+                console.error('Weird tx !', rawTransaction);
+                return false;
             }
-            return false;
+            try {
+                return parseInt(rawTransaction.vout[0].amount) !== 0;
+            }
+            catch (err) {
+                return false;
+            }
         };
         TransactionsExplorer.parse = function (rawTransaction, wallet) {
             var transaction = null;
