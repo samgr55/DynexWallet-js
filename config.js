25d00227485058c3dc18e69a6797d432c73b9558
Samer Rahmeh <samgr93@gmail.com>
2023-08-11 09:05:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 var global = typeof window !== 'undefined' ? window : self;
 global.config = {
     nodeList: [
-        "https://node.dynexcoin.org:18334/"
+        "http://node.dynexcoin.org:18333/"
         
     ],
     nodeUrl: "",
